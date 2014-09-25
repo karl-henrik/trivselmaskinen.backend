2cea2b82df1b72d6259c4bcd6b62409c2495593d
johag <jon.haglof@sogeti.se>
2014-09-25 13:57:33 +0200

    Added SQL for deleting content from all tables

---
+++ delete_from_all_tables.sql
@@ -0,0 +1,8 @@
+DELETE FROM DeviceLayer.RawValues;
+DELETE FROM ServiceLayer.QuestionScenarioDevices;
+DELETE FROM ServiceLayer.Devices;
+DELETE FROM ServiceLayer.QuestionScenarios;
+DELETE FROM ServiceLayer.QuestionTypeValues;
+DELETE FROM ServiceLayer.QuestionTypes;
+DELETE FROM ServiceLayer.Locations;
+DELETE FROM ServiceLayer.Questions;
